e1da5b851781cd84237518c66e332b06457b37b3
Denis <den.kov.2015@gmail.com>
2023-12-04 18:10:26 +0200

    WIP: Trying to make it work

--- app/_components/forms/create-form.tsx
+++ app/_components/forms/create-form.tsx
@@ -1,5 +1,49 @@
 import { createService } from "@/app/_lib/actions";
 
+const serviceForm = {
+    inputs: [
+        {
+            label: "Service Name",
+            inputType: "text",
+            inputId: "name"
+        },
+        {
+            label: "Service Cost",
+            inputType: "number",
+            inputId: "cost"
+        }
+    ]
+}
+
+const placeForm = {
+    inputs: [
+        {
+            label: "Place Name",
+            inputType: "text",
+            inputId: "name"
+        },
+        {
+            label: "Place Address",
+            inputType: "text",
+            inputId: "address"
+        },
+        {
+            label: "Place Description",
+            inputType: "text",
+            inputId: "description"
+        },
+        {
+            label: "Place Services",
+            inputType: "select",
+            inputId: "services",
+        },
+        {
+            label: "Place Image",
+            inputType: "file",
+            inputId: "image",
+        }
+    ]
+}
 
 export default function CreateForm(){
     return(
